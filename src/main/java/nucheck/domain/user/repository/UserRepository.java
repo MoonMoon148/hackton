321901553e91ab7a00fa7381511b5d189500f24f
Moon <gyibok17@gmail.com>
2026-08-19 16:07:29 +0900

    Delete src directory

--- src/main/java/nucheck/domain/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package nucheck.domain.user.repository;
-
-import nucheck.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,String> {
-}
